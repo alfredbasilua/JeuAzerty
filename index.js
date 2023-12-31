705b8355226e5b5ec406e93c6b93d33b23bc6401
alfredbasilua <alfredbasiluabda@gmail.com>
2023-12-31 23:25:41 +0100

    couleur

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-let zoneProposition= document.getElementById("zoneProposition");
-console.log(zoneProposition.clientHeight);
-let listeInputRadio = document.querySelectorAll(".zoneChoix input");
+let contenuTitre="Bonjour"
+let contenuparagraphe="comment vous allez?"
+
+let nouveauTitre=document.createElement("h1")
+let nouveauparagraphe=document.createElement("p")
+let nouvellediv=document.createElement("div")
+
+nouveauTitre.textContent=contenuTitretitre
+nouveauparagraphe.textContent=contenuparagraphe
+
+nouvellediv.appendChild(contenuTitre)
+nouvellediv.appendChild(contenuparagraphe)
+let body=document.querySelector("body")
+body.innerHTML=div
+body.appendChild(nouvellediv)
